57b5b8724368e65931448723382c148d6422857f
Wheslley Rimar <wheslley_rimar@hotmail.com>
2022-06-01 14:45:59 -0300

    Backend Sistema de Coworking

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const routes = require('./routes');
+const app = express();
+
+mongoose.connect('mongodb+srv://user:senha@cluster0.ogyfz.mongodb.net/app?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.disable("x-powered-by");
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3333;
+}
+app.listen(port);
